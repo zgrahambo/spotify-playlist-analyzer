4ac0634a12267d937120f7d719fe534f09513b3b
Zachary Graham <zgraham@scu.edu>
2019-06-08 16:19:03 -0700

    add export

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import 'semantic-ui-css/semantic.min.css';
 import PlaylistAnalyzerApp from './PlaylistAnalyzerApp/PlaylistAnalyzerApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<PlaylistAnalyzerApp/>, document.getElementById('root'));
+
+export default function renderSpotifyPlaylistAnalyzer() {
+  ReactDOM.render(<PlaylistAnalyzerApp/>, document.getElementById('root'));
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
